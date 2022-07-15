eb55b8f29b7284d8e52e0649926fd9031009c2b8
Xavier <xavier.tnc@gmail.com>
2022-07-15 10:03:45 +0200

    F1Site V1.15.3 - FT - F1.Form JS Class - Add unhappyClass, errorsClass, focus, focusError, clear, reset and init!

--- public_html/js/form.js
+++ public_html/js/form.js
@@ -1,4 +1,4 @@
-/* global window, console, F1 */
+/* globals F1 */
 
 /**
  * F1 Form JS
@@ -13,16 +13,15 @@
 
 
 function extend( obj, props ) { 
-  for ( let key in props || {} ) { obj[ key ] = props[ key ]; } 
-}
+  for ( let key in props || {} ) { obj[ key ] = props[ key ]; } }
 
 
-function clearErrors() {
-  console.log( 'Clear Errors', this );
-  this.errors = [];
-  this.elm.classList.add( 'cleared' );
-  this.elm.classList.remove( 'unhappy' );
-  this.elm.querySelectorAll( '.messages' ).forEach(
+function clearErrors( frmObj, errorsSelector ) {
+  console.log( 'Clear Errors:', frmObj );
+  frmObj.errors = [];
+  frmObj.elm.classList.remove( frmObj.unhappyClass );
+  errorsSelector = errorsSelector || '.' + frmObj.errorsClass.replace( ' ', '.' );
+  frmObj.elm.querySelectorAll( errorsSelector ).forEach(
     elMsgs => elMsgs.parentElement.removeChild( elMsgs ) );
 }
 
@@ -32,6 +31,8 @@ const Form = function( options )
   const defaults = {
     selector: 'form',
     fieldSelector: '.field',
+    unhappyClass: 'unhappy',
+    errorsClass: 'errors',
     fieldTypes: {},
     validators: {},
     fields: []
@@ -40,7 +41,16 @@ const Form = function( options )
   extend( this, options );
   this.elm = document.querySelector( this.selector || 'form' );
   this.getFields();
+  if ( this.initialValues )
+    this.init( this.initialValues );
 };
+
+
+Form.Error = function( elField, message )
+{
+  this.elField = elField;
+  this.message = message;
+}
 
 
 Form.FieldType = function( id, options )
@@ -52,47 +62,40 @@ Form.FieldType = function( id, options )
 
 Form.FieldType.prototype = {
 
-  getInputs: function()
-  {
-    const field = this;
+  getInputs: function() { const field = this;
     const elements = this.elm.querySelectorAll( this.inputSelector );
     elements.forEach( elm => field.inputs.push( elm ) );
   },
 
-  getName: function() { return this.input.name; },
-  getValue: function() { return this.input.value; },
-  getLabel: function() {
-    let label = this.elm.getAttribute( 'aria-label' );
-    if ( ! label && this.input ) {
-      label = this.input.getAttribute( 'aria-label' ); 
-    } else {
-      const elLbl = this.elm.querySelector( 'label' );
+  getLabel: function() { let label = this.elm.getAttribute( 'aria-label' );
+    if ( ! label && this.input ) { label = this.input.getAttribute( 'aria-label' ); }
+    else { const elLbl = this.elm.querySelector( 'label' );
       if ( ! label && elLbl ) { label = elLbl.innerHTML; } 
-      else if ( ! label ) { label = this.getName(); }
-    }
+      else if ( ! label ) { label = this.getName(); } }
     return label || 'Field';
   },
+
+  getName: function() { return this.input.name; },
+  getValue: function() { return this.input.value; },
   getRequired: function() { return this.elm.classList.contains( 'required' ); },
   setValue: function( val ) { this.input.value = val; },
+  clearErrors: function() { clearErrors( this ) },
+  clear: function() { this.setValue( '' ); },
 
-  showErrors: function()
-  {
+  showErrors: function() {
     if ( !this.errors.length ) return;
     const elMsgs = document.createElement( 'div' );
-    elMsgs.innerHTML = this.errors.map( e => '<div class="error">'+e+'</div>' ).join('');
-    elMsgs.className = 'messages errors';
-    this.elm.classList.add( 'unhappy' );
+    elMsgs.innerHTML = this.errors.map( e => '<div class="error">'+e.message+'</div>' ).join('');
+    elMsgs.className = this.errorsClass;
+    this.elm.classList.add( this.unhappyClass );
     this.elm.appendChild( elMsgs );
   },
 
-  clearErrors,
-
-  validate: function( options )
-  {
+  validate: function( options ) {
     const validator = this.form.validators.Required;
     const valid = validator.test( this, options );
-    if ( ! valid ) this.errors.push( 
-      validator.getInvalidMessage( this, options ) );
+    if ( ! valid ) this.errors.push( new Error( this.elm,
+      validator.getInvalidMessage( this, options ) ) );
     return valid;
   },
 
@@ -107,6 +110,8 @@ Form.Field = function( form, elm )
   this.form = form;
   this.inputs = [];
   this.errors = [];
+  this.errorsClass = form.errorsClass;
+  this.unhappyClass = form.unhappyClass;
   const fieldTypeId = elm.getAttribute( 'data-type' );
   const fieldType = form.fieldTypes[ fieldTypeId ];
   extend( this, fieldType || new Form.FieldType( 'Text' ) );
@@ -131,16 +136,12 @@ Form.Validator.prototype = {
 
 Form.prototype = {
 
-  addField: function( elm )
-  {
-    this.fields.push( new Form.Field( this, elm ) );
-  },
+  addField: function( elm ) {
+    this.fields.push( new Form.Field( this, elm ) ); },
 
-  getFields: function()
-  {
+  getFields: function() {
     const elements = this.elm.querySelectorAll( this.fieldSelector );
-    elements.forEach( elm => this.addField( elm ) );
-  },
+    elements.forEach( elm => this.addField( elm ) ); },
 
   getErrors: function() {
     const errors = [];
@@ -149,22 +150,38 @@ Form.prototype = {
     return errors;
   },
 
-  showErrors: function()
-  {
+  showErrors: function() {
     const errors = [];
-    this.elm.classList.add( 'unhappy' );
+    this.elm.classList.add( this.unhappyClass );
     this.fields.forEach( field => { field.showErrors(); 
       if ( field.errors[0] ) errors.push( field.errors[0] ); } );
     const elMsgs = document.createElement( 'div' );
-    elMsgs.className = 'messages summary';
-    elMsgs.innerHTML = errors.map( e => '<div class="error">'+e+'</div>' ).join('');
+    elMsgs.className = this.errorsClass + ' summary';
+    elMsgs.innerHTML = errors.map( e => '<div class="error">'+e.message+'</div>' ).join('');
     this.elm.appendChild( elMsgs );
   },
 
-  clearErrors, 
+  clearErrors: function() { clearErrors( this, '.summary' );
+    this.fields.forEach( field => field.clearErrors() ); },
 
   validate: function( options ) { this.fields.forEach( field => 
     { field.clearErrors(); field.validate( options ) } ); },
+
+  focus: function() { this.elm.focus(); },
+
+  focusError: function( focusLast ) {
+    const errors = this.getErrors();
+    if ( errors.length > 0 ) {
+      const errIndex = focusLast ? errors.length : 0;
+      errors[ errIndex ].elField.focus(); }
+  },
+
+  clear: function() { this.clearErrors();
+    this.fields.forEach( field => field.clear() ); },
+
+  reset: function() { return this.elm.reset(); },
+
+  init: function() {},
 
 };
 
