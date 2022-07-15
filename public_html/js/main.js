11fa8d39407014327f01f5901f0ecdf41d6f0815
Xavier <xavier.tnc@gmail.com>
2022-07-15 10:04:57 +0200

    F1Site V1.15.4 - FT - main.js: Added `F1.console` Now we log errors with F1.console.log()

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,6 +2,8 @@
  * On Page Ready
  */
 
-if ( F1.DEBUG ) { console.log( F1 ); }
+F1.console = window.console || { log: function(){} };
+
+if ( F1.DEBUG ) { F1.console.log( F1 ); }
 
 F1.deferred.forEach( fn => fn() );
