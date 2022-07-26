6d5a3f0137ba4aae227dd3490753c1c5bbe18ebe
Xavier <xavier.tnc@gmail.com>
2022-07-26 12:42:30 +0200

    F1Site V1.17.3 - DEV - Main.js: Wrap in DOMContentLoaded event handler.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,9 +1,5 @@
-/**
- * On Page Ready
- */
-
-F1.console = window.console || { log: function(){} };
-
-if ( F1.DEBUG ) { F1.console.log( F1 ); }
-
-F1.deferred.forEach( fn => fn() );
+/** On Page Ready */
+document.addEventListener( 'DOMContentLoaded', function() {
+  F1.deferred.forEach( fn => fn() );
+  if ( F1.DEBUG ) console.log( F1 );
+});
